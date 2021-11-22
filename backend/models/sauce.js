59616e9de615d2954d87e4bf66a0099ebb4223fd
Matthieu Thepaut <matth.thepautpgmail.com>
2021-11-22 15:17:54 +0100

    Création de la logique de base

---
+++ backend/models/sauce.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const sauceSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainePepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Sauce", sauceSchema);
